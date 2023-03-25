eb908fe6d5cf574869638e228a604245cd3a88d8
Alejandro Palacios <alexpalagomez_1255@hotmail.com>
2023-03-24 20:04:07 -0500

    unnecesary code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -52,19 +52,3 @@ function Home() {
 
 export default Home;
 
-{/* <div class="flex justify-around ">
-      <div class="grid grid-cols-2 text-center">
-        <div class="grid grid-rows-4">
-          <div>I'm</div>
-          <div>Alejandro Palacios Gomez</div>
-          <div>Fullstack Web Devloper</div>
-          <div class="flex justify-around">
-            <div>Hire me!</div>
-            <div>Contact me!</div>
-          </div>
-        </div>
-        <div>
-          <img src={selfie} alt="selfie" />
-        </div>
-      </div>
-    </div> */}
