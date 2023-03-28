42d19922bf21bf9257e3de17cb25fdb00eb2afab
Alejandro Palacios <alexpalagomez_1255@hotmail.com>
2023-03-27 22:56:31 -0500

    Formulario de contacto

---
+++ src/componentes/MailComponent.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+        try {
+      
+      setIsModalOpen(true);
+      setFormData({
+        name: '',
+        email: '',
+        message: '',
+      });
+    } catch (error) {
+      console.error(error);
+      alert('Ha ocurrido un error al enviar tu mensaje.');
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="max-w-lg mx-auto mt-10 h-screen">
+        <div className="mb-4">
+          <label htmlFor="name" className="block mb-2 font-bold text-gray-700">Nombre</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:ring-blue-400"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="block mb-2 font-bold text-gray-700">Correo electrónico</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:ring-blue-400"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="block mb-2 font-bold text-gray-700">Mensaje</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="w-full px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:ring-blue-400"
+            required
+          />
+        </div>
+        <div className="text-center">
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-400">Enviar</button>
+        </div>
+      </form>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Mensaje enviado"
+        className="modal absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-lg p-4"
+        overlayClassName="overlay"
+      >
+        <h2 className="text-2xl font-bold mb-2">Mensaje enviado</h2>
+        <p className="mb-4">Tu mensaje ha sido enviado correctamente.</p>
+        <button onClick={() => setIsModalOpen(false)} className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-400">Cerrar</button>
+      </Modal>
+    </>
+  );
+};
+
+export default ContactForm;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,68 +1,88 @@
-import {React,Fragment } from "react";
-import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { React, Fragment } from "react";
+import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import ContactForm from "../componentes/MailComponent";
 
 function Contact() {
   return (
     <Fragment>
-    <header className="bg-gray-900">
-      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold leading-tight text-white">
-          Contáctame
-        </h1>
-        <p className="mt-1 text-gray-300">
-          Puedes encontrarme en estas plataformas:
-        </p>
-      </div>
-    </header>
-    <div className="bg-gray-100 py-8">
-      <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="bg-white overflow-hidden shadow rounded-lg">
-            <div className="px-4 py-5 sm:p-6">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <FaEnvelope className="h-6 w-6 text-gray-400" />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    Email
+      <header className="bg-gray-900">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold leading-tight text-white">
+            Contáctame
+          </h1>
+          <p className="mt-1 text-gray-300">
+            Puedes encontrarme en estas plataformas:
+          </p>
+        </div>
+      </header>
+      <div className="bg-gray-100 py-8">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="bg-white overflow-hidden shadow rounded-lg">
+              <div className="px-4 py-5 sm:p-6">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <FaEnvelope className="h-6 w-6 text-gray-400" />
                   </div>
-                  <div className="text-sm text-gray-500">
-                    <a href="mailto:alexpalagomez_1255@hotmail.com " target="_blank"  rel="noopener noreferrer">alexpalagomez_1255@hotmail.com</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-white overflow-hidden shadow rounded-lg">
-            <div className="px-4 py-5 sm:p-6">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <FaLinkedin className="h-6 w-6 text-gray-400" />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    LinkedIn
-                  </div>
-                  <div className="text-sm text-gray-500">
-                    <a href="https://www.linkedin.com/in/alejandro-palacios-gomez-6a1292172/" target="_blank"  rel="noopener noreferrer">Alejandro-palacios-gomez-6a1292172</a>
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-gray-900">
+                      Email
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      <a
+                        href="mailto:alexpalagomez_1255@hotmail.com "
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        alexpalagomez_1255@hotmail.com
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bg-white overflow-hidden shadow rounded-lg">
-            <div className="px-4 py-5 sm:p-6">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <FaGithub className="h-6 w-6 text-gray-400" />
-                </div>
-                <div className="ml-4">
-                  <div className="text-sm font-medium text-gray-900">
-                    GitHub
+            <div className="bg-white overflow-hidden shadow rounded-lg">
+              <div className="px-4 py-5 sm:p-6">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <FaLinkedin className="h-6 w-6 text-gray-400" />
                   </div>
-                  <div className="text-sm text-gray-500">
-                    <a href="https://github.com/Alexpalagomez1255" target="_blank"  rel="noopener noreferrer">Alexpalagomez1255</a>
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-gray-900">
+                      LinkedIn
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      <a
+                        href="https://www.linkedin.com/in/alejandro-palacios-gomez-6a1292172/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Alejandro-palacios-gomez-6a1292172
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white overflow-hidden shadow rounded-lg">
+              <div className="px-4 py-5 sm:p-6">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <FaGithub className="h-6 w-6 text-gray-400" />
+                  </div>
+                  <div className="ml-4">
+                    <div className="text-sm font-medium text-gray-900">
+                      GitHub
+                    </div>
+                    <div className="text-sm text-gray-500">
+                      <a
+                        href="https://github.com/Alexpalagomez1255"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Alexpalagomez1255
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -70,8 +90,17 @@ function Contact() {
           </div>
         </div>
       </div>
-    </div>
-  </Fragment>
+      <div className="bg-gray-100">
+      <div className="flex justify-center items-center">
+        <div className=" mx-6 text-lg font-bold text-gray-900 text-center bg-white overflow-hidden shadow-lg rounded-lg p-1">
+          Deja tus datos a continuación y me pondré en contacto contigo
+        </div>
+      </div>
+      <div >
+        <ContactForm />
+      </div>
+      </div>
+    </Fragment>
   );
 }
 
